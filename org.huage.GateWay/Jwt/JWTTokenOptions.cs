6664d0315a07a707aafa405ca95d5da084456722
jing <jingxinpeng@kingagroot.com>
2024-01-24 11:05:53 +0800

    Initial commit

---
+++ org.huage.GateWay/Jwt/JWTTokenOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace org.huage.GateWay.Jwt;
+
+public class JWTTokenOptions
+{
+    public string Audience { get; set; }
+    public string Isuser { get; set; }
+    public string SecurityKey { get; set; }
+}
